fa5ddd0e71f685d5034bbc45122a67c6f6dc0c4d
Dexctor <120388125+Dexctor@users.noreply.github.com>
2023-12-04 14:01:31 +0100

    Update multer-config.js

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -6,7 +6,8 @@ const fs = require('fs');
 const MIME_TYPES = {
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
-    'image/png': 'png'
+    'image/png': 'png',
+    'image/webp': 'webp'
 };
 
 const storage = multer.diskStorage({
@@ -24,28 +25,29 @@ const upload = multer({storage: storage}).single('image');
 
 const resizeImage = (req, res, next) => {
   if (!req.file) {
-    return next();
+      return next();
   }
 
   const filePath = req.file.path;
   const fileName = req.file.filename;
-  const outputFilePath = path.join(__dirname, '../images', `resized_${fileName}`);
+  const outputFilePath = path.join(__dirname, '../images', `resized_${fileName.split('.')[0]}.webp`); // Changement de l'extension en .webp
 
   sharp(filePath)
-    .resize({ width: 206, height: 260 })
-    .toFile(outputFilePath)
-    .then(() => {
-      fs.unlink(filePath, () => {
-        req.file.path = outputFilePath;
-        req.file.filename = `resized_${fileName}`;
-        console.log('sharp success');
-        next();
+      .resize({ width: 206, height: 260 })
+      .toFormat('webp') // Conversion en WebP
+      .toFile(outputFilePath)
+      .then(() => {
+          fs.unlink(filePath, () => {
+              req.file.path = outputFilePath;
+              req.file.filename = `resized_${fileName.split('.')[0]}.webp`; // Mise à jour du nom de fichier
+              console.log('Image convertie en webp');
+              next();
+          });
+      })
+      .catch(err => {
+          console.error('Sharp error:', err);
+          return next();
       });
-    })
-    .catch(err => {
-      console.error('Sharp error:', err);
-      return next();
-    });
 };
 
 module.exports = { upload, resizeImage };
